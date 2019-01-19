af292338b97dcdcaac3d1e960ea8b00891f69ebc
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2019-01-19 18:50:33 -0500

    Roman to Integer

---
+++ Leetcode/13.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int cnt=map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(map.get(s.charAt(i+1))>map.get(s.charAt(i))){
+                cnt-=map.get(s.charAt(i));
+            }else{
+                cnt+=map.get(s.charAt(i));
+            }
+        }
+        return cnt;
+    }
+}

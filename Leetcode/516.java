4687969cb6f238dff052b224318d9d8db94e9e30
ViVaHa <varshath@Vishnus-MBP.fios-router.home>
2019-02-26 20:51:04 -0500

    Longest Palindromic Subsequence

---
+++ Leetcode/516.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int dp[][] = new int[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            dp[i][i] = 1;
+        }
+        for(int len=1;len<s.length();len++){
+            for(int i=0;i<s.length()-len;i++){
+                int j = i+len;
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1]+2;
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][s.length()-1];
+    }
+}
